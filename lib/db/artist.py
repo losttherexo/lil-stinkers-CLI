88dcc7f853e2c038fbbb4b5962ad113b798f4f59
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-27 15:43:30 -0400

    Revert "made new files for classes - organizational purposes"
    This reverts commit 1ddea774b2356ac1dabafa9a99fac360b5c26ffe.

--- lib/db/artist.py
+++
@@ -1,8 +0,0 @@
-class Artist:
-    
-    all_artists = []
-
-    def __init__(self, name, genre):
-        self.name = name
-        self.genre = genre
-        Artist.all_artists.append(self)
--- lib/db/listener.py
+++
@@ -1,7 +0,0 @@
-class Listener:
-
-    all_listeners = []
-
-    def __init__(self, name,):
-        self.name = name
-        Listener.all_listeners.append(self)
