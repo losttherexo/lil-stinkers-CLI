c61fafb0907b4b625bd6bd0c29b480fd96e545e0
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-27 17:12:38 -0400

    setup for sqlalchemy

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,26 +1,35 @@
-class Artist:
+from sqlalchemy import (create_engine, Column, Integer, String)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+engine = create_engine('')
+Session = sessionmaker(bind = engine)
+session = Session()
+
+Base = declarative_base()
+
+class Artist(Base):
+    __tablename__ = 'artists'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
     
-    all_artists = []
 
-    def __init__(self, name, genre):
-        self.name = name
-        self.genre = genre
-        Artist.all_artists.append(self)
+class Listener(Base):
+    __tablename__ = 'listeners'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
 
-class Listener:
 
-    all_listeners = []
+class Song(Base):
+    __tablename__ = 'songs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
 
-    def __init__(self, name,):
-        self.name = name
-        Listener.all_listeners.append(self)
+Base.metadata.create_all(engine)
 
-class Song:
 
-    all_songs = []
-
-    def __init__(self, name, artist_instance, listener_instance):
-        self.name = name
-        self.artist = artist_instance
-        self.listeners = listener_instance
-        Song.all_songs.append(self)
+# artist = Artist(name='Mac Miller')
+# session.add(artist)
+# session.commit()
