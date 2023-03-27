1e422b5cc4d71505205a4237c1e0cba7d4b149aa
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-03-27 12:31:20 -0400

    basic classes setup and instances in debug file

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,8 +1,26 @@
 class Artist:
     
-    all = []
+    all_artists = []
 
     def __init__(self, name, genre):
         self.name = name
         self.genre = genre
-        Artist.all.append(self)
+        Artist.all_artists.append(self)
+
+class Listener:
+
+    all_listeners = []
+
+    def __init__(self, name,):
+        self.name = name
+        Listener.all_listeners.append(self)
+
+class Song:
+
+    all_songs = []
+
+    def __init__(self, name, artist_instance, listener_instance):
+        self.name = name
+        self.arist = artist_instance
+        self.listeners = listener_instance
+        Song.all_songs.append(self)
---
+++ lib/debug.py
@@ -0,0 +1,12 @@
+# need to import models.py - unsure on how to do so
+
+A1 = Artist("Mac Miller", "Rap")
+A2 = Artist("Ludwig van Beethoven", "Classical")
+
+L1 = Listener("Jesse")
+L2 = Listener("Andre")
+L3 = Listener("Tom")
+L4 = Listener("Collin")
+
+S1 = Song("Ladders", A1, L4)
+S2 = Song("Für Elise", A2, L1)
